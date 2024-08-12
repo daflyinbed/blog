63f4eb13856149f28ab5db36b6c8cf14a38e0c04
daflyinbed <1677759063@qq.com>
2024-08-12 23:05:40 +0800

    fix: update title

--- src/site.config.ts
+++ src/site.config.ts
@@ -22,7 +22,7 @@ export const siteConfig: SiteConfig = {
   // Option to sort posts by updatedDate if set to true (if property exists). Default (false) will sort by publishDate
   sortPostsByUpdatedDate: false,
   // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-  title: "Astro Theme Cactus",
+  title: "夕舞八弦",
 };
 
 // Used to generate links in both the Header & Footer.
